a26a547eb9fb4aae82ee56fe67c302ecbe36974f
take-null <bluerose.pandra@gmail.com>
2022-03-30 16:42:14 +0900

    fix commit
    create

---
+++ back/app/controllers/api/v1/hello_controller.rb
@@ -0,0 +1,5 @@
+class Api::V1::HelloController < ApplicationController
+  def index
+    render json: "افتح يا سمسم"
+  end
+end
---
+++ back/config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root 'application#hello'
+  namespace 'api' do
+    namespace 'v1' do
+      # api test action
+      resources :hello, only:[:index]
+    end
+  end
+end
