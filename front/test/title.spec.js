4e2195be9ac02ef5f7d607bd18d6c25a21f3706f
take-null <bluerose.pandra@gmail.com>
2022-03-30 17:16:48 +0900

    second commit

---
+++ front/test/title.spec.js
@@ -0,0 +1,9 @@
+import { mount } from '@vue/test-utils'
+import Hello from '@/pages/index'
+
+describe('open Sesame', () => {
+  test('display open Sesame', () => {
+    const wrapper = mount(Hello)
+    expect(wrapper.text()).toBe("open Sesame")
+  })
+})
