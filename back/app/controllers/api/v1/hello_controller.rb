fd10dfe5a3981976ac485812d7df404019b4e9d8
take-null <bluerose.pandra@gmail.com>
2022-04-02 12:56:11 +0900

    t

--- back/app/controllers/api/v1/hello_controller.rb
+++
@@ -1,5 +0,0 @@
-class Api::V1::HelloController < ApplicationController
-  def index
-    render json: "افتح يا سمسم"
-  end
-end
--- back/app/controllers/application_controller.rb
+++
@@ -1,5 +0,0 @@
-class ApplicationController < ActionController::API
-    def hello
-        render json: "HELL"
-    end
-end
