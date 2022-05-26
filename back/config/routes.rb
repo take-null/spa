ffffddf4862e1b0ac89f3b7591a2ddd011d6d17d
take-null <bluerose.pandra@gmail.com>
2022-05-26 15:59:40 +0900

    modified routes

--- back/config/routes.rb
+++ back/config/routes.rb
@@ -2,15 +2,11 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth', controllers: {
     registrations: 'auth/registrations'
   }
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  #root 'application#hello'
-  #namespace 'api' do
-  #  namespace 'v1' do
-  #   api test action
-  #    resources :hello, only:[:index]
-  #  end
-  #end
+  #以下は動作テスト用のルーティング
+  root 'application#hello'
+  namespace 'api' do
+    namespace 'v1' do
+      resources :hello, only:[:index]
+    end
+  end
 end
