821315172908cfb9abf39a0e800bdd7747998a56
take-null <bluerose.pandra@gmail.com>
2022-06-20 22:09:13 +0900

    create login page

---
+++ front/pages/login.vue
@@ -0,0 +1,65 @@
+<template>
+  <before-login-form-card>
+    <template #form-card-content>
+      <v-form
+        ref="form"
+        v-model="isValid"
+      >
+        <user-form-email 
+          :email.sync="params.auth.email"
+          no-validation
+        />
+        <user-form-password 
+          :password.sync="params.auth.password"
+          no-validation
+        />
+        <v-card-actions>
+          <nuxt-link
+            to="#"
+            class="body-2 text-decoration-none"
+        >
+          パスワードを忘れた？
+        </nuxt-link>
+        </v-card-actions>
+        <v-card-text class="px-0">
+          <v-btn
+            :disabled="!isValid || loading"
+            :loading="loading"
+            block
+            color="blue"
+            class="white--text"
+            @click="login"
+          >
+            ログインする
+          </v-btn>
+        </v-card-text>
+      </v-form>
+      <v-card-text>
+        {{ params }}
+      </v-card-text>
+    </template>
+  </before-login-form-card>
+</template>
+
+<script>
+export default {
+  layout: 'beforeLogin',
+  data () {
+    return {
+      isValid: false,
+      loading: false,
+      params: { auth: { email: '', password: '' } }
+    }
+  },
+  methods: {
+    login () {
+      this.loading = true
+      setTimeout(() => {
+        this.$store.dispatch('login')
+        this.$router.replace('/')
+        this.loading = false
+      }, 1500)
+    }
+  }
+};
+</script>
