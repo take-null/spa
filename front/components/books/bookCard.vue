f4da42080d8f67569161cc38d2c9f564feedaa21
take-null <bluerose.pandra@gmail.com>
2022-08-03 18:30:33 +0900

    fix UI

--- front/components/books/bookCard.vue
+++ front/components/books/bookCard.vue
@@ -2,22 +2,32 @@
   <v-card 
     color="blue-grey lighten-5"
     class="mx-auto"
-    max-width="426"
-    height="280"
+    max-width="300"
+    @click="openDialogChild"
   >
-   <v-container>
-    <v-row dense>
-      <v-col cols="6">
+    <v-container
+      fluid
+    >
+      <v-row 
+        no-gutters
+      >
+        <v-col>
           <v-list-item-content>
-            <v-list-item class="d-flex flex-column justify-content-start">
-              <template v-if="image === null">
+            <v-list-item 
+              class="d-flex flex-column justify-content-start"
+            >
+              <template 
+                v-if="image === null"
+              >
                 <v-img
                   max-height="250"
                   max-width="150"
                   :src="require('@/assets/img/20200505_noimage.jpg')"
                 />
               </template>
-              <template v-else>
+              <template 
+                v-else
+              >
                 <v-img
                   max-heigth="250"
                   max-width="150"
@@ -26,35 +36,32 @@
               </template>
             </v-list-item>
           </v-list-item-content>
-      </v-col>
-      <v-col cols="6">
-        <v-list-item three-line>
-        <v-list-item-content class="d-flex justify-content-end">
-          <div class="text-sm-h7 mb-4">
-            {{title}}
-          </div>
-          <v-list-item-subtitle>
-            作者:{{authors}}
-          </v-list-item-subtitle>
-          <v-list-item-subtitle>
-            出版社:{{publisher}}
-          </v-list-item-subtitle>
-          <v-list-item-subtitle>
-            出版年月日:{{published_at}}
-          </v-list-item-subtitle>
-        </v-list-item-content>
-        </v-list-item>
-        <div class="d-flex justify-content-end">
-          <v-btn
-            @click="openDialogChild"
-            dark
-            class="white--text"
+        </v-col>
+        <v-col>
+          <v-list-item 
+            three-line
           >
-            <v-icon>mdi-pencil-plus</v-icon>
-          </v-btn>
-        </div>
-      </v-col>
-    </v-row>
+            <v-list-item-content 
+              class="d-flex justify-content-end"
+            >
+              <div 
+                class="text-body-1 mb-4"
+              >
+                {{title}}
+              </div>
+              <v-list-item-subtitle>
+                作者:{{authors}}
+              </v-list-item-subtitle>
+              <v-list-item-subtitle>
+                出版社:{{publisher}}
+              </v-list-item-subtitle>
+              <v-list-item-subtitle>
+                出版年月日:{{published_at}}
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-col>
+      </v-row>
     </v-container>
   </v-card>
 </template>
--- front/pages/contents/_id/books.vue
+++ front/pages/contents/_id/books.vue
@@ -1,5 +1,7 @@
 <template>
-  <v-container fluid>
+  <v-container 
+    fluid
+  >
     <v-card
       flat
       tile
@@ -9,19 +11,23 @@
       <v-card-title>
         本を探す
       </v-card-title>
+      <v-card-text>
+        本を選択してレビューを書くとタイムラインと本棚に投稿データが追加されます
+      </v-card-text>
       <v-form
         ref="form"
-        v-model="isValid">
+        v-model="isValid"
+      >
           <v-text-field
             v-model="keyword"
             clearable
             text
             label="本の名前を入力してください"
-              placeholder="本を検索"
-              type="text"
-              :rules="rules"
-              @keypress.enter.prevent="eventReady ? setBooks() : null"
-            >
+            placeholder="本を検索"
+            type="text"
+            :rules="rules"
+            @keypress.enter.prevent="eventReady ? setBooks() : null"
+          >
             <template v-slot:append>
               <v-col
                 cols="12"
@@ -44,36 +50,54 @@
         </v-text-field>
       </v-form>
     </v-card>
-
     <v-dialog
       v-model="postMessage"
-      max-width="426"
     >
       <v-card
         color="cyan lighten-4"
-        >
+      >
         <v-list-item-content>
-          <v-list-item-title class="text-h5 mb-1 cyan--text" >
-            <v-icon>mdi-checkbox-marked-circle-plus-outline</v-icon>レビューを投稿しました！
+          <v-list-item-title 
+            class="text-h5 mb-1 cyan--text" 
+          >
+            <v-icon>
+              mdi-checkbox-marked-circle-plus-outline
+            </v-icon>
+            レビューを投稿しました！
           </v-list-item-title>
-          <v-list-item-subtitle class="cyan--text">{{title}}が</v-list-item-subtitle>
-          <v-list-item-subtitle class="cyan--text">本棚に追加されます。</v-list-item-subtitle>
+          <v-list-item-subtitle 
+            class="cyan--text"
+          >
+            {{title}}が
+          </v-list-item-subtitle>
+          <v-list-item-subtitle 
+            class="cyan--text"
+          >
+            本棚に追加されます。
+          </v-list-item-subtitle>
         </v-list-item-content>
       </v-card>
     </v-dialog>
 
-    <v-dialog v-model="dialog"  max-width="426">
+    <v-dialog 
+      v-model="dialog"
+      max-width="600"
+    >
       <v-card
         color="blue-grey lighten-5"
         class="mx-auto"
       >
-      <v-container>
-        <v-row dense>
-          <v-col cols="6">
-              <v-list-item class="d-flex flex-column justify-content-start">
+        <v-container>
+          <v-row 
+            no-gutters
+          >
+            <v-col>
+              <v-list-item 
+                class="d-flex flex-column justify-content-start"
+              >
                 <template v-if="image === null">
                   <img
-                    height="230"
+                    height="250"
                     width="150"
                     src="@/assets/img/20200505_noimage.jpg"
                   >
@@ -86,59 +110,71 @@
                   />
                 </template>
               </v-list-item>
-          </v-col>
-          <v-col cols="6">
-            <v-list-item three-line>
-              <v-list-item-content class="d-flex justify-content-end">
-                <div class="text-sm-h7 mb-4">
-                  {{title}}
-                </div>
-                <v-list-item-subtitle>
-                  作者:{{authors}}
-                </v-list-item-subtitle>
-                <v-list-item-subtitle>
-                  出版社:{{publisher}}
-                </v-list-item-subtitle>
-                <v-list-item-subtitle>
-                  出版年月日:{{published_at}}
-                </v-list-item-subtitle>
-              </v-list-item-content>
-            </v-list-item>
-          </v-col>
-        </v-row>
-        <v-row>
-        <v-form
-          ref="form"
-          v-model="isValid"
-          class="pa-4 pt-0"
-        >
-          <v-card-subtitle>
-            本を評価(５段階)
-          </v-card-subtitle>
-          <v-rating
-            v-model="rating"
-            background-color="orange lighten-3"
-            color="orange"
-            x-small
-          ></v-rating>
-          <v-card-subtitle>
-            タグを追加(５つまで)
-          </v-card-subtitle>
-            <v-combobox
-              v-model="model"
-              :filter="filter"
-              :hide-no-data="!search"
-              :items="items"
-              :search-input.sync="search"
-              hide-selected
-              label="先頭から最大５つまで、タグが登録されます"
-              multiple
-              small-chips
-              solo
+            </v-col>
+            <v-col>
+              <v-list-item 
+                three-line
               >
-              <template v-slot:no-data>
-                <v-list-item>
-                  <span class="subheading">Create</span>
+                <v-list-item-content 
+                  class="d-flex justify-content-end"
+                >
+                  <div 
+                    class="text-body-1 mb-4"
+                  >
+                    {{title}}
+                  </div>
+                  <v-list-item-subtitle>
+                    作者:{{authors}}
+                  </v-list-item-subtitle>
+                  <v-list-item-subtitle>
+                    出版社:{{publisher}}
+                  </v-list-item-subtitle>
+                  <v-list-item-subtitle>
+                    出版年月日:{{published_at}}
+                  </v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-form
+              ref="form"
+              v-model="isValid"
+              class="pa-4 pt-0"
+            >
+              <v-card-subtitle>
+                本を評価(５段階)
+              </v-card-subtitle>
+              <v-rating
+                v-model="rating"
+                background-color="orange lighten-3"
+                color="orange"
+                x-small
+              ></v-rating>
+              <v-card-subtitle>
+                タグを追加(５つまで)
+              </v-card-subtitle>
+                <v-combobox
+                  v-model="model"
+                  :filter="filter"
+                  :hide-no-data="!search"
+                  :items="items"
+                  :search-input.sync="search"
+                  hide-selected
+                  label="先頭から最大５つまで、タグが登録されます"
+                  multiple
+                  small-chips
+                  solo
+                >
+                <template 
+                  v-slot:no-data
+                >
+                  <v-list-item>
+                    <span 
+                      class="subheading"
+                    >
+                      Create
+                    </span>
                     <v-chip
                       :color="`${colors[nonce - 1]} lighten-3`"
                       label
@@ -146,18 +182,22 @@
                     >
                       {{ search }}
                     </v-chip>
-                </v-list-item>
-              </template>
-              <template v-slot:selection="{ attrs, item, parent, selected }">
-                <v-chip
-                  v-if="item === Object(item)"
+                  </v-list-item>
+                </template>
+                <template 
+                  v-slot:selection="{ attrs, item, parent, selected }"
+                >
+                  <v-chip
+                    v-if="item === Object(item)"
                     v-bind="attrs"
                     :color="`${item.color} lighten-3`"
                     :input-value="selected"
                     label
                     small
-                >
-                  <span class="pr-2">
+                  >
+                  <span 
+                    class="pr-2"
+                  >
                     {{ item.text }}
                   </span>
                   <v-icon
@@ -166,8 +206,8 @@
                   >
                     $delete
                   </v-icon>
-              </v-chip>
-              </template>
+                  </v-chip>
+                </template>
               <template v-slot:item="{ index, item }">
                 <v-text-field
                   v-if="editing === item"
@@ -189,56 +229,64 @@
                   {{ item.text }}
                 </v-chip>
                 <v-spacer></v-spacer>
-                <v-list-item-action @click.stop>
+                <v-list-item-action 
+                  @click.stop
+                >
                   <v-btn
                     icon
                     @click.stop.prevent="edit(index, item)"
                   >
                   <v-icon>{{ editing !== item ? 'mdi-pencil' : 'mdi-check' }}</v-icon>
                   </v-btn>
-                </v-list-item-action>
-              </template>
-            </v-combobox>
-          <v-textarea
-            ref="comment"
-            :rules="[() => !!comment || 'コメントを入力して下さい']"
-            v-model="comment"
-            clearable
-            clear-icon="mdi-close-circle"  
-            label="コメントを書く"
-            required
-          >
-          </v-textarea>
-        </v-form>
-        </v-row>
-      <v-divider></v-divider>
-      <v-card-actions>
-        <v-btn
-          text
-          @click="dialog = false"
-        >
-          close
-        </v-btn>
-        <v-spacer></v-spacer>
-        <v-btn
-          :disabled="!isValid || loading"
-          :loading="loading"
-          class="white--text"
-          dark
-          depressed
-          @click.stop="postBook"
-        >
-          投稿
-        </v-btn>
-      </v-card-actions>
-      </v-container>
-    </v-card>
+                  </v-list-item-action>
+                </template>
+              </v-combobox>
+              <perfect-scrollbar>
+                <v-textarea
+                  ref="comment"
+                  :rules="[() => !!comment || 'レビューを入力して下さい']"
+                  v-model="comment"
+                  clearable
+                  clear-icon="mdi-close-circle"  
+                  label="レビューを書く"
+                  required
+                >
+                </v-textarea>
+              </perfect-scrollbar>
+            </v-form>
+          </v-row>
+          <v-divider></v-divider>
+          <v-card-actions>
+            <v-btn
+              text
+              @click="dialog = false"
+            >
+              close
+            </v-btn>
+            <v-spacer></v-spacer>
+            <v-btn
+              :disabled="!isValid || loading"
+              :loading="loading"
+              class="white--text"
+              dark
+              depressed
+              @click.stop="postBook"
+            >
+              投稿
+            </v-btn>
+          </v-card-actions>
+        </v-container>
+      </v-card>
     </v-dialog>
-    <v-container fluid>
-      <v-row dense>
+    <v-container 
+      fluid
+    >
+      <v-row 
+        dense
+      >
         <v-col
           v-for="book in books" :key="book.google_books_api_id"
-          cols="12" sm="6" md="6" lg="4" xl="3"
+          cols="12" sm="6" md="4" lg="3" xl="2"
         >
           <bookCard
             :google_books_api_id.sync="book.google_books_api_id"
@@ -255,10 +303,16 @@
     </v-container>
   </v-container>
 </template>
+
 <script>
 import bookCard from '~/components/books/bookCard.vue'
+import { PerfectScrollbar } from 'vue2-perfect-scrollbar'
+import 'vue2-perfect-scrollbar/dist/vue2-perfect-scrollbar.css'
 export default {
-  components: { bookCard },
+  components: { 
+                bookCard,
+                PerfectScrollbar
+              },
   methods: {
     async setBooks () {
       try {
@@ -434,3 +488,10 @@ export default {
   },
 }
 </script>
+
+<style>
+.ps {
+  height: 150px;
+  border: 1px solid #212121;
+}
+</style>
