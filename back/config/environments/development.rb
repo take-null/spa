c57bcf07bee8d2c631917a3068a82d37ae4d98c9
take-null <bluerose.pandra@gmail.com>
2022-08-29 22:37:02 +0900

    fix database.yml

--- back/config/environments/development.rb
+++ back/config/environments/development.rb
@@ -76,6 +76,5 @@ Rails.application.configure do
     user_name: ENV['EMAIL_ADDRESS'],
     password: ENV['EMAIL_PASSWORD'],
     authentication: :plain,
-    enable_starttls_auto: true
   }
 end
--- back/config/environments/production.rb
+++ back/config/environments/production.rb
@@ -98,6 +98,5 @@ Rails.application.configure do
     user_name: ENV['EMAIL_ADDRESS'],
     password: ENV['EMAIL_PASSWORD'],
     authentication: :plain,
-    enable_starttls_auto: true
   }
 end
