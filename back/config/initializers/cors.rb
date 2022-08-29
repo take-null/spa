a243745d91dadf5c36ead118f0c3d30c5f693982
take-null <bluerose.pandra@gmail.com>
2022-08-29 18:56:18 +0900

    test cors

--- back/config/initializers/cors.rb
+++ back/config/initializers/cors.rb
@@ -7,11 +7,12 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    if Rails.env.production?
-      origins 'https://referer-hub.com'
-    else  
-      origins 'http://localhost:8080'
-    end
+    #if Rails.env.production?
+      #origins 'https://referer-hub.com'
+    #else  
+      #origins 'http://localhost:8080'
+    #end
+    origins '*'
 
      resource "*",
        headers: :any,
