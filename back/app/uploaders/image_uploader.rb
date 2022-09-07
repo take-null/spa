3e04546b89c9f6d0b42e9c1e7cc6820edc7460c3
take-null <bluerose.pandra@gmail.com>
2022-09-07 17:08:07 +0900

    fix image_uploader for aws

--- back/app/uploaders/image_uploader.rb
+++ back/app/uploaders/image_uploader.rb
@@ -4,8 +4,15 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  #storage :file
   # storage :fog
+  if Rails.env.development?
+    storage :file
+  elsif Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -45,7 +52,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+  def filename
+    original_filename if original_filename
+  end
 end
---
+++ back/config/initializers/carrierwave.rb
@@ -0,0 +1,13 @@
+unless Rails.env.development? || Rails.env.test?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.credentials.s3[:aws_access_key_id],
+      aws_secret_access_key: Rails.application.credentials.s3[:aws_secret_access_key],
+      region: 'ap-northeast-1'
+    }
+  
+    config.fog_directory  = Rails.application.credentials.s3[:fog_directory]
+    config.cache_storage = :fog
+  end
+end
